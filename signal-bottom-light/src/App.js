5e6fa268f25f23dcc3d92edc3bbf2ccc5a011bb1
heejin <desireoftruth1126@gmail.com>
2026-01-18 14:31:26 +0900

    signal-bottom-light

---
+++ signal-bottom-light/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SignalBottomLight from "./components/SignalBottomLight";
+
+const App = () => {
+  return <SignalBottomLight />;
+};
+
+export default App;
